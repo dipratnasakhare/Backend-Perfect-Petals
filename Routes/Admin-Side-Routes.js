b855aae4da61ae9a14cc4406ed2ac3c20a9de05f
dipratnasakhare <dipratnasakhare11@email.com>
2023-02-27 10:01:03 +0530

    adding backend for work is done

--- Routes/Admin-Side-Routes.js
+++ Routes/Admin-Side-Routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ModelAdminTask } = require("../models/AdminTask");
 const { ModelUserAuth } = require("../models/UserAuth.Models");
 const { ModelOrderDetails } = require("../models/UserOrderDetails");
 const AdminSideRoutes = express.Router();
@@ -6,7 +7,7 @@ const AdminSideRoutes = express.Router();
 AdminSideRoutes.get("/users", async (req, res) => {
   try {
 
-    let All_User = await ModelUserAuth.find({ user_type: "Client" });
+    let All_User = await ModelUserAuth.find();
     res.status(200).send({ count: All_User.length });
   } catch (err) {
     console.log(err, "err line 20");
@@ -26,33 +27,20 @@ AdminSideRoutes.post("/OrderPost", async (req, res) => {
   try {
     let User_Details = await ModelOrderDetails.find({ UserId });
 
-    if (User_Details.length > 0) {
-      User_Arr = User_Details[0].OrderDetails;
-      let newdata = req.body.OrderDetails[0];
-      user_total = User_Details[0].TotalPrice;
-      let TotalPrice = req.body.TotalPrice + user_total;
-
-      await ModelOrderDetails.updateOne({ UserId }, { $set: { TotalPrice } });
-      await ModelOrderDetails.updateOne(
-        { UserId },
-        { $push: { OrderDetails: newdata } }
-      );
-      await ModelOrderDetails.updateOne(
-        { UserId },
-        { $set: { Status: "progress" } }
-      );
-
-      res
-        .status(200)
-        .send({ msg: "Order history is updated", status: "success" });
-    } else {
-      let User_Data = new ModelOrderDetails(req.body);
-      User_Data.save();
-
-      res
-        .status(200)
-        .send({ msg: "Order history is created", status: "success" });
+    if(User_Details.length == 0){
+      req.body.Status = "Progress"
+      let save = new ModelOrderDetails(req.body);
+      await save.save()
+    }else{
+      User_Details[0].TotalPrice += req.body.TotalPrice
+      User_Details[0].OrderDetails = [...User_Details[0].OrderDetails, ...req.body.OrderDetails]
+     let x = await ModelOrderDetails.updateOne({ UserId }, User_Details[0]);
     }
+
+   res
+   .status(200)
+   .send({ msg: "Order history is updated", status: "success" })
+
   } catch (err) {
     console.log(err, "err line 20");
 
@@ -62,11 +50,34 @@ AdminSideRoutes.post("/OrderPost", async (req, res) => {
   }
 });
 
+
+
+AdminSideRoutes.post("/GetOrderForUser", async (req, res) => {
+  const { UserId } = req.body;
+  try {
+    let Order_Details = await ModelOrderDetails.find({ UserId });
+        res
+        .status(200)
+        .send({ Order_Details });
+  } catch (err) {
+    console.log(err, "err line 20");
+    res
+      .status(200)
+      .send({ msg: "Something went wrong please try again", status: "error" });
+  }
+});
+
+
+
+
 AdminSideRoutes.get("/OrderGet", async (req, res) => {
-  const { page = 1, limit = 3 } = req.query;
+  const { page = 1, limit = 1 } = req.query;
 
   try {
     let length = await ModelOrderDetails.find();
+    if(limit == 1){
+      res.send({orderList:length});
+    }
     let data = await ModelOrderDetails.find()
     .limit(limit * 1)
     .skip((page - 1) * limit);
@@ -77,7 +88,6 @@ AdminSideRoutes.get("/OrderGet", async (req, res) => {
   });
   } catch (err) {
     console.log(err, "err line 20");
-
     res
       .status(200)
       .send({ msg: "Something went wrong please try again", status: "error" });
@@ -99,10 +109,144 @@ AdminSideRoutes.post("/Admin-get", async (req, res) => {
 });
 
 
+AdminSideRoutes.post("/Get-Admin-TaskList", async (req, res) => {
+  let { UserId } = req.body
+  try {
+    let list =  await ModelAdminTask.findOne({ AdminId: UserId });
+      res
+      .status(200)
+      .send({ list: list.AdminTask })
+  } catch (err) {
+    console.log(err, "err line 20");
+    res
+      .status(200)
+      .send({ msg: "Something went wrong please try again", status: "error" });
+  }
+});
+
+AdminSideRoutes.post("/Delete-Admin-TaskList", async (req, res) => {
+  let { UserId , id} = req.body
+  try {
+    let list =  await ModelAdminTask.find({ AdminId: UserId });
+    list = list[0]
+    const d = list.AdminTask.filter((ele) => `${ele["_id"]}` !==  id)
+    list.AdminTask = d
+    await ModelAdminTask.updateOne({ AdminId: UserId }, list);
+      res
+      .status(200)
+      .send({ msg: "Task is deleted", status: "success" })
+  } catch (err) {
+    console.log(err, "err line 20");
+    res
+      .status(200)
+      .send({ msg: "Something went wrong please try again", status: "error" });
+  }
+});
+
+AdminSideRoutes.post("/Toggle-Admin-TaskList", async (req, res) => {
+
+  let { UserId , id} = req.body
+
+  try {
+    let list =  await ModelAdminTask.find({ AdminId: UserId });
+    list = list[0]
+    list.AdminTask.filter((ele, i) => {
+      if(`${ele["_id"]}` ==  id){
+        ele.Status = !ele.Status
+        console.log(`${ele["_id"]}` ==  id,ele)
+      }
+    })
+    await ModelAdminTask.updateOne({ AdminId: UserId }, list);
+      res
+      .status(200)
+      .send({ msg: "Task is updates", status: "success" })
+  } catch (err) {
+    console.log(err, "err line 20");
+    res
+      .status(200)
+      .send({ msg: "Something went wrong please try again", status: "error" });
+  }
+});
+
+AdminSideRoutes.post("/Update-Admin-TaskList", async (req, res) => {
+  let { UserId , data, id} = req.body
+  try {
+    let list =  await ModelAdminTask.find({ AdminId: UserId });
+    list = list[0]
+    list.AdminTask.filter((ele, i) => {
+      if(`${ele["_id"]}` ==  id){
+        list.AdminTask[i] = data
+      }
+    })
+    await ModelAdminTask.updateOne({ AdminId: UserId }, list);
+      res
+      .status(200)
+      .send({ msg: "Task is updates", status: "success" })
+  } catch (err) {
+    console.log(err, "err line 20");
+    res
+      .status(200)
+      .send({ msg: "Something went wrong please try again", status: "error" });
+  }
+});
 
 
 
+AdminSideRoutes.post("/Admin-TaskList", async (req, res) => {
+  let { UserId, data } = req.body
 
+  data.Status = false
+  try {
+    let list =  await ModelAdminTask.findOne({ AdminId: UserId });
+
+    if(typeof(list) == "object"){
+      let list =  await ModelAdminTask.updateOne({ AdminId: UserId }, {$push: {AdminTask: data}});
+      res
+      .status(200)
+      .send({ msg: "task is already", status: "success" })
+    }
+
+    if(list == null){
+     const AdminList = {
+      AdminId: UserId, 
+      AdminTask: [data]
+     }
+      let pata =  new ModelAdminTask(AdminList);
+      pata.save()
+      res
+      .status(200)
+      .send({ msg: "task is added", status: "success" })
+    }
+
+  } catch (err) {
+    console.log(err, "err line 20");
+    res
+      .status(200)
+      .send({ msg: "Something went wrong please try again", status: "error" });
+  }
+});
+
+
+// 
+
+AdminSideRoutes.post("/get-my-account", async (req, res) => {
+  let { UserId } = req.body
+
+  try {
+    let data =  await ModelUserAuth.find({ UserId });
+    data = data[0]
+    data.password = "*********"
+      res
+      .status(200)
+      .send({ data })
+
+  } catch (err) {
+    console.log(err, "err line 20");
+    res
+      .status(200)
+      .send({ msg: "Something went wrong please try again", status: "error" });
+  }
+});
 
 
 
